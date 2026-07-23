492210ca8cf0fbd3858f8349ca31b62f21299ae8
mentordayeeta <dayeetacloud@gmail.com>
2026-07-23 15:56:31 +0000

    July 23

---
+++ Dayeeta/July 23/Output.java
@@ -0,0 +1,38 @@
+// Output
+
+class Output{
+    public static void main(String[] abc){
+        System.out.println("Next Line!");
+        System.out.print("Hello!");
+        System.out.print("Hello!");
+        System.out.print("Hello!");
+    }
+}
+
+// Single Line Comments
+
+/* Multi
+Line 
+Comments */
+
+//Variables/Data Types
+
+//Integer
+int x; //Initialize
+int y = 5; //Declare
+
+//String
+String a;
+String z = "Shresth";
+
+//Long
+//Float
+//Double
+//Array
+int[] m = {20};
+
+//Character
+//Short
+//Byte
+//Boolean
+
