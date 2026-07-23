6eee21de26c4ce90f4276f80489f9b524766115c
mentordayeeta <dayeetacloud@gmail.com>
2026-07-23 15:14:57 +0000

    QNA

--- Dayeeta/July 22/New.java
+++ Dayeeta/July 22/New.java
@@ -3,3 +3,13 @@ public static void main(String[] args) {
 System.out.println("Hello World!");
 }
 }
+
+// Question for Shayan
+// Can a single java file have multiple classes?
+
+// Answer ->
+// Yes, you Can
+// 1) Only one public class allowed per file
+// 2) The public class should have the same name as the file
+// 3) Other classes will have a default access modifier.
+// 4) All classes will have seperate .class
