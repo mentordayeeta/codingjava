c5c076b72a35c088d0c8dfb6be3347a9fd18a028
GojiraGuy2014 <albh3421@gmail.com>
2026-07-25 15:24:32 +0000

    Temprature yayyyy hot cold yipppie

---
+++ Temprature.java
@@ -0,0 +1,9 @@
+class Temprature {
+    public static void main(String[] args) {
+        double Cel=40;
+        double Fah=(Cel*9/5)+32;
+        double Kel=Cel+273.15;
+        System.out.println(Fah);
+        System.out.println(Kel);
+    }
+}
