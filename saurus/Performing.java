b0275fb85bb2a84dd25f5e6b0dbdb4571cbebaac
GojiraGuy2014 <albh3421@gmail.com>
2026-07-25 15:38:06 +0000

    action

---
+++ saurus/Performing.java
@@ -0,0 +1,10 @@
+class Performing {
+    public static void main(String[] args) {
+        int A=67;
+        int i=61;
+        System.out.println(A+i);
+        System.out.println(A-i);
+        System.out.println(A*i);
+        System.out.println(A/i);
+    }
+}
