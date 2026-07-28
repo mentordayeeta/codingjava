b4a8dd96520fd22ab96f7feb584f5d323ef28470
GojiraGuy2014 <albh3421@gmail.com>
2026-07-28 15:24:05 +0000

    A type of data

---
+++ Aalabhya/TypesofData.java
@@ -0,0 +1,11 @@
+public class TypesofData {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println(num);
+        System.out.println(num+num);
+        System.out.println(num-num);
+        System.out.println(num*num);
+        System.out.println(num/num);
+    }
+    
+}
