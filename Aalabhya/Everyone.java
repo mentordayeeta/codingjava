06b0b460c66974ef4d7b9e3099ac9d5136a28e77
GojiraGuy2014 <albh3421@gmail.com>
2026-07-29 20:57:23 +0530

    EVERYONE IS HERE

---
+++ Aalabhya/Everyone.java
@@ -0,0 +1,23 @@
+import javax.swing.plaf.TreeUI;
+
+public class Everyone {
+    public static void main(String[] args) {
+        int number=5;
+        System.out.println(number);
+        String stringy="Hi!";
+        System.out.println(stringy);
+        float floaty=5;
+        System.out.println(floaty);
+        long lengthy=5;
+        System.out.println(lengthy);
+        short shorty=5;
+        System.out.println(shorty);
+        char character='A';
+        System.out.println(character);
+        boolean MHM=true;
+        System.out.println(MHM);
+
+
+    }
+    
+}
