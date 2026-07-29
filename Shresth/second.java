dd863049d9e304c9ab0eec322c1411e2c1bf539b
Shresth <shresthgoyal1316x@gmail.com>
2026-07-29 21:35:26 +0530

    second

---
+++ Shresth/second.java
@@ -0,0 +1,20 @@
+class second{
+public static void main(String[] args){
+int num = 5;
+System.out.println(num);
+String r = "hello";
+System.out.println(r);
+float b = 67;
+System.out.println(num);
+double c = 9.1967;
+System.out.println(r);
+char d = 's';
+System.out.println(num);
+boolean s = true;
+System.out.println(r);
+System.out.println(b - num);
+System.out.println(b - c);
+System.out.println(b + c);
+System.out.println(b + num);
+System.out.println(num + c);
+}}
