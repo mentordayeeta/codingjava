024bf50b2dd3b4d1d295a5582d97c945d396e14c
Shresth <shresthgoyal1316x@gmail.com>
2026-07-29 21:12:25 +0530

    ronaldo

---
+++ Shresth/shresth.java
@@ -0,0 +1,15 @@
+class shresth{
+public static void main(String[] args){
+int num = 5;
+System.out.println(num);
+String r = "hello";
+System.out.println(r);
+float n = 67;
+System.out.println(n);
+double s = 9.1967;
+System.out.println(s);
+char c = 's';
+System.out.println(c);
+boolean b = true;
+System.out.println(b);
+}}
