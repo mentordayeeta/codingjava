141381df7a1023ebc13581b6e6e2f8557ff3198c
Shayan Raza <shayanendgameavengers@gmail.com>
2026-07-30 21:12:48 +0530

    u19

--- Shayan/Fibonacci.java
+++ Shayan/Fibonacci.java
@@ -1,4 +1,21 @@
 // Write a Program to Find Sum of Fibonacci Series Number
 // Input n or take n
 // Fibonacci number at that positon
-
+import java.util.*;
+class Fibonacci 
+{
+    public static void main(String args[])
+    {
+        Scanner obj = new Scanner (System.in);
+        int a=0,b=1,n,sum=0;
+        System.out.println("Enter a number");
+        n=obj.nextInt();
+        for (int i=1;i<=n;i++)
+        {
+            sum=a+b;
+            a=b;
+            b=a;
+        }
+        System.out.println(sum);
+    }
+}
