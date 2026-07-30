b114c3346625fe5988660bb7997f53fa1088217a
GojiraGuy2014 <albh3421@gmail.com>
2026-07-30 21:12:24 +0530

    SWITCHAROO

---
+++ Aalabhya/SWAP.java
@@ -0,0 +1,13 @@
+public class SWAP {
+    public static void main(String[] args) {
+        int A=67;
+        int B=61;
+        System.out.println(A);
+        System.out.println(B);
+        int C=A;
+        A=B;
+        B=C;
+        System.out.println(A);
+        System.out.println(B);
+    }
+}
