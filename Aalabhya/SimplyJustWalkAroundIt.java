28d98e3b9c8c08a46639c9b79fd8506a5f9cc456
GojiraGuy2014 <albh3421@gmail.com>
2026-07-30 20:58:58 +0530

    If a Roblox Wall is One Stud Thick,you can simply just walk around it

---
+++ Aalabhya/SimplyJustWalkAroundIt.java
@@ -0,0 +1,9 @@
+public class SimplyJustWalkAroundIt {
+    public static void main(String[] args) {
+        int Money=500;
+        int Rate=10;
+        int Time=5;
+        int Intrest=Money*Rate*Time/100;
+        System.out.println(Intrest);
+    }
+}
