b8e2dcb43e33ff85ae6ce19ca0b184620a70eb86
Shresth <shresthgoyal1316x@gmail.com>
2026-08-04 21:18:52 +0530

    Swap case

---
+++ Shresth/Swap.java
@@ -0,0 +1,12 @@
+class Swap{
+public static void main(String[] args){
+    int a = 67;
+    int b = 69;
+    System.out.println(a);
+    System.out.println(b);
+    int c = a;
+        a = b;
+        b = c;
+    System.out.println(a);
+    System.out.println(b);
+}}
