16ea0eb89335464f45e6a525ccbc3b15748f3b14
Shresth <shresthgoyal1316x@gmail.com>
2026-08-04 21:07:46 +0530

    division

--- Shresth/Division.java
+++ Shresth/Division.java
@@ -1,2 +1,9 @@
 // Take values: divisor, and dividend
 // Print: quotient and remainder
+class Division{
+public static void main(String[] args){
+int Divisor = 7;
+int Dividend = 44;
+System.out.println(Dividend % Divisor);
+System.out.println(Dividend / Divisor);
+}}  
