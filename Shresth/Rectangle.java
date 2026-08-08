02c510ac0506ae7f09b2897b685e2550dd1dffe6
Shresth <shresthgoyal1316x@gmail.com>
2026-08-08 21:36:10 +0530

    rectangle

---
+++ Shresth/Rectangle.java
@@ -0,0 +1,10 @@
+class Rectangle{
+public static void main(String[] hello){
+    double breadth = 99.8756d;
+    double length = 999.869384d;
+    double perimeter = 2*(length + breadth);
+    double area = length*breadth;
+    System.out.println(perimeter);
+    System.out.println(area);
+}
+}
--- ro.py
+++ ro.py
@@ -1,1 +1,9 @@
-p = input("Enter your number less the 10: ")
+Time = int(input("Enter your time in minutes : "))
+count = 0
+if Time<60:
+    print(Time,"minutes")
+else:
+    while Time>=60:
+        Time -= 60
+        count +=1
+    print(count,"hours,",Time,"Minutes")
