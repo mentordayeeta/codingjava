cd71a8283a23266d217aaaa2fe07f8ca340a948e
GojiraGuy2014 <albh3421@gmail.com>
2026-08-24 21:37:18 +0530

    Cubibism

---
+++ Aalabhya/Inputs and Methods/Cube.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Cube {
+    public static void main(String[] godxilla) {
+        Scanner var = new Scanner(System.in);
+        System.out.println("Enter a number!:");
+        int var1 = var.nextInt();
+        System.out.println(var1 * var1 * var1);
+
+    }
+}
