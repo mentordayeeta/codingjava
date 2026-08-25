121a6d892f83cc7efa1ae24b9616338e791e445e
GojiraGuy2014 <albh3421@gmail.com>
2026-08-25 20:43:14 +0530

    OPERATE

---
+++ Aalabhya/Inputs and Methods/Multiplication.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Multiplication {
+    public static void main(String[] GODZILLA) {
+        Scanner var = new Scanner(System.in);
+        System.out.println("Enter a number!:");
+        int var1 = var.nextInt();
+        System.out.println("Enter a number!:");
+        int vary1 = var.nextInt();
+        System.out.println(var1 * vary1);
+        System.out.println(var1 / vary1);
+        System.out.println(var1 + vary1);
+        System.out.println(var1 - vary1);
+    }
+}
