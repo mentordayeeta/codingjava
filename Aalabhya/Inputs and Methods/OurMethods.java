f28c6034ee130a578be01bde604e3f4e9469fa95
mentordayeeta <dayeetacloud@gmail.com>
2026-08-26 15:37:45 +0000

    User Defined Methods

---
+++ Aalabhya/Inputs and Methods/OurMethods.java
@@ -0,0 +1,22 @@
+public class OurMethods {
+
+    //Creating a simple Method
+    // public static void addition(){
+    //     int a = 8;
+    //     int b = 10;
+    //     System.out.println(a+b);
+    // }
+    public static void multiply(int x, int y){
+        System.out.println(x*y);
+    }
+
+    public static void main(String[] args) {
+        // addition(); // Calling a method
+        System.out.println("My name is Dayeeta");
+        // addition();
+        multiply(8, 10);
+        multiply(20, 10);
+        multiply(30, 190);
+    }
+}
+
