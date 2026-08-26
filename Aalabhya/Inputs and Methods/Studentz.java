62a621d11c3feec11a1b80ff12276ec0909c10de
GojiraGuy2014 <albh3421@gmail.com>
2026-08-26 21:20:18 +0530

    STUDENT

---
+++ Aalabhya/Inputs and Methods/Studentz.java
@@ -0,0 +1,14 @@
+public class Studentz {
+    // COMMENT.My name is Aalabhya.I study in MDVM Parle School.My role no is 1.My
+    // sr no is 5005.My class is 7th grade.
+    public static void data(String name, String school, int role, int sr, int grade) {
+        System.out.println("My name is " + name + ".My grade is " + grade + ".My school is " + school + ".My SR is "
+                + sr + "My Role is " + role);
+
+    }
+
+    public static void main(String[] DELTARUNE) {
+        data("Aalabhya", "MDVM PARLE", 1, 5005, 7);
+    }
+
+}
