0208c827e1b377ddbde5016e4fdb3c105d040eaf
GojiraGuy2014 <albh3421@gmail.com>
2026-09-07 20:47:36 +0530

    health

--- Aalabhya/Healthbar.java
+++ Aalabhya/Healthbar.java
@@ -1,1 +1,15 @@
-public class Heal
+public class Healthbar{
+        public static void main(String[] args) {
+        int Hp=100;
+        int Dmg=20;
+        int x=1;
+        while(Hp!=0){
+            System.out.println("You have "+Hp+" health!");
+            System.out.println("You have taken "+x+" hits!");
+            Hp-=Dmg;
+            x+=1;
+
+        }
+        System.out.println("You died...");
+}
+}
