dab62e2276f8f0486ded5ef274f6888b2ffa5305
mentordayeeta <dayeetacloud@gmail.com>
2026-09-07 16:00:08 +0000

    SquareStar

---
+++ Dayeeta/Sep 7/SquareStar.java
@@ -0,0 +1,15 @@
+public class SquareStar {
+    public static void main(String[] args) {
+        int star=6;
+        while (star!=0){
+            System.out.println("* * * * * *");
+            star-=1;
+        }
+    }
+}
+
+*
+**
+***
+****
+*****
