7cb6f5860d9a95221c492800b2791ac8e009c9c0
GojiraGuy2014 <albh3421@gmail.com>
2026-09-07 20:53:54 +0530

    Rejuvinate the Rotten

--- Aalabhya/Healthbar.java
+++ Aalabhya/Healthbar.java
@@ -11,5 +11,17 @@ public class Healthbar{
 
         }
         System.out.println("You died...");
+                Hp=Dmg*3;
+        x=1;
+        System.out.println("You now have revived!Hp="+Hp);
+            while(Hp!=0){
+            System.out.println("You have "+Hp+" health!");
+            System.out.println("You have taken "+x+" hits!");
+            Hp-=Dmg;
+            x+=1;
+
+        }
+        System.out.println("You've died again...");
+
 }
 }
