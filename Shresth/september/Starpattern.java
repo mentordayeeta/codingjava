0fc70796150b0744b5b2639d3794bdd27b97584d
Shresth <shresthgoyal1316x@gmail.com>
2026-09-08 21:38:46 +0530

    Starpattern

---
+++ Shresth/september/Starpattern.java
@@ -0,0 +1,22 @@
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+class Starpattern{
+    public static void main(String[] args){
+        int y = 1;
+        System.out.println(y);
+        int r = 22;
+        System.out.println(r);
+        int w = 333;
+        System.out.println(w);
+        int e = 4444;
+        System.out.println(e);
+        int d = 55555;
+        System.out.println(d);
+        int u = 666666;
+        System.out.println(u);
+    }
+}
---
+++ Shresth/september/Starpattern.py
@@ -0,0 +1,12 @@
+# *
+# **
+# ***
+# ****
+# *****
+# ******
+r = int(input("Enter any number "))
+for i in range(0,r):
+    for j in range(0,i):
+        print("*",end="")
+    print()
+
