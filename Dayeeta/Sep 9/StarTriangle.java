a3e9db17e6f7c81c441c2b92eaef6262428d7de2
mentordayeeta <dayeetacloud@gmail.com>
2026-09-09 15:39:34 +0000

    For Loops

---
+++ Dayeeta/Sep 9/StarTriangle.java
@@ -0,0 +1,14 @@
+public class StarTriangle {
+    public static void main(String[] args) {
+        int x=8;
+        for(int i=1;i<=x;i++){
+            System.out.println(i);
+        }
+    }
+}
+
+//FizzBuzz
+
+//loop from 0 to 100 ->print
+//if number is divisible by 3 -> print "Fizz" instead of the number
+//if number is divisible by 5 -> print "Buzz" instead of the number
